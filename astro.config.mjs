75bbfb892c5211365a5b5e91c13464bda549aa57
Pol Quintana <pol.quintana1@gmail.com>
2024-06-02 10:46:54 +0200

    Update for GH Pages deploy to new.getsolt.pro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://new.getsolt.pro",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
